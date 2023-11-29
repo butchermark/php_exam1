784e8b2a6f7bec68e003a1fc6e0d275889cbe2f3
unknown <meszaros1127@gmail.com>
2023-11-29 18:35:23 +0100

    init

---
+++ services/functions.php
@@ -0,0 +1,11 @@
+<?php
+
+function printMenu()
+{
+    echo '<a href="../pages/index.php">Home</a>
+    <a href="../pages/services.php">Services</a>
+<a href="../pages/projects.php">Projects</a>
+    <a href="../pages/contact.php">Contact</a';
+}
+
+?> 
---
+++ services/navbar.php
@@ -0,0 +1,30 @@
+<?php include '../services/functions.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Beadando</title>
+
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+	<link href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,400;0,600;0,700;0,900;1,400;1,600;1,700&display=swap" rel="stylesheet" />
+
+	<link rel="stylesheet" href="../res/style.css" />
+</head>
+<body>
+<header>
+		<div class="container">
+			<h2><span>web</span>designer</h2>
+			<nav>
+                <?php printMenu(); ?>
+			</nav>
+		</div>
+	</header>
+	<main>
+		<section class="banner">
+			
+		</section>
+    </main>
+    
+</body>
+</html>
